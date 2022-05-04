45bc6ef27f3601b2d4f9a69c474203a123163eea
bladeinblood <101563891+bladeinblood@users.noreply.github.com>
2022-05-04 11:55:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,52 @@
 import discord
 from discord.ext import commands
+from discord_components import *
+import asyncio
+
+PREFIX = 'f.'
+
+client = commands.Bot(command_prefix=PREFIX)
+client.remove_command('help')
+
+@client.event
+async def on_ready():
+  print('Bot connect')
+  
+@client.command()
+async def ban(ctx, member: discord.Member, message, reason = None):
+  await ctx.send(f'{member.mention} was banned')
+  await asyncio.sleep(2)
+  await ctx.message.delete()
+  await member.ban(reason=reason)
+  
+@client.command()
+async def mute(ctx, message, member: discord.Member):
+  await ctx.send("Test Select Menu", 
+  components =
+  [Select(placeholder = "Choose what you want to see!",
+         options = [
+            SelectOption(
+              label = "Option 1",
+              value = "Option 1, woho",
+              description = "Option 1, my dear developer",
+              emoji = '😀'
+            ),
+           SelectOption(
+              label = "Option 2",
+              value = "Option 2, woho",
+              description = "Option 2, my dear developer",
+              emoji = '😀'
+            ),
+           SelectOption(
+              label = "Option 3",
+              value = "Option 3, woho",
+              description = "Option 3, my dear developer",
+              emoji = '😀'
+            ),
+         ])])
+  e1 = discord.Embed(title = 'Embed 1', description = 'embed woho')
+  e2 = discord.Embed(title = 'Embed 2', description = 'embed woho')
+  e3 = discord.Embed(title = 'Embed 3', description = 'embed woho')
+  
+  
+client.run('token here')
